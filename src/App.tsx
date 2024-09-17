1dfec3115d3b7b41b5f9c5c6645db44c59945a16
Axel-Notebook <axelkevinagustinrojas@gmail.com>
2024-09-17 05:49:53 -0300

    quick type fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState } from "react";
 import { SearchForm } from "./SearchForm";
 import { SearchResultItem } from "./SearchResultItem";
